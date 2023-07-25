fba8c604c694536a3bf95340aaa574b5d85b4a22
Elmangacadiabolico <111751667+Elmangacadiabolico@users.noreply.github.com>
2023-07-25 11:44:45 -0300

    dwa
    sdwasdawz

---
+++ script.js
@@ -0,0 +1,15 @@
+const tone = require('tone')
+
+function preload() {
+    song = loadSound("/pianito./sound/C Piano.mp3");
+}
+
+const btn = document.getElementById("selectKey");
+btn.addEventListener("click", () => {});
+
+
+function sound () {
+
+
+    
+}
